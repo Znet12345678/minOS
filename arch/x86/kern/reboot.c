e96a3f7f49ea1c1520d7abe4be436605e7d0842a
System Administrator <root@Zachs-Mac-mini.home>
2016-01-22 17:38:54 -0600

    Fixed reboot thing

--- arch/x86/kern/reboot.c
+++ arch/x86/kern/reboot.c
@@ -1,11 +1,12 @@
 #include <stdint.h>
 void reboot(){
-	/*uint8_t good = 0x02;
+	debug("KERNEL","Halting System NOW!");
+	uint8_t good = 0x02;
 	while(good & 0x02)
 		good = inb(0x64);
 	outb(0x64,0xFE);
-	asm volatile("hlt");*/
-	debug("KERNEL","hlt");
-	debug("KERNEL","hang");
-	while(1){ };
+	asm volatile("hlt");
+	//debug("KERNEL","hlt");
+	//debug("KERNEL","hang");
+	//while(1){ };
 }
