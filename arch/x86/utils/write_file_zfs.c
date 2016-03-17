ad6bb8e41f5d655fa8816fc3d94b7eb3734cf706
znet12345678 <znet12345678@gmail.com>
2016-03-17 18:21:02 -0500

    Fixed stuff still not done with zfs

--- arch/x86/utils/write_file_zfs.c
+++ arch/x86/utils/write_file_zfs.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <string.h>
 #include <zfs.h>
+#include <fcntl.h>
+struct zfs_superblock *parse_superblock(char *fname,struct zfs_superblock *sblk);
 int get_end(const char *fname){
 	FILE *f;
 	if(!(f = fopen(fname,"rb"))){
@@ -22,7 +24,7 @@ int get_end(const char *fname){
 }
 int zero_out(const char *fname){
 	FILE *f;
-	if(!(f = fopen(fname,"wb"))){
+	if(!(f = fopen(fname,"r+b"))){
 		printf("Couldn't open %s for writing",fname);
 	}
 	int end = get_end(fname);
@@ -31,14 +33,15 @@ int zero_out(const char *fname){
 	fclose(f);
 	return 1;
 }
-int find_datablk(struct zfs_superblock *sblk,const char *fname){
-	int fds = sblk->fds;
+int find_datablk(struct zfs_superblock sblk,const char *fname){
+	//int fds = sblk.fds;
 	FILE *f;
 	if(!(f = fopen(fname,"rb"))){
 		printf("Couldn't open file:%s for reading\n",fname);
 		return -1;
 	}
-	fseek(f,fds * 512,SEEK_SET);
+	printf("[sblk.fds] %d\n",sblk.fds);
+	fseek(f,sblk.fds * 512,SEEK_SET);
 	int i = 0;
 	int c = 0;
 	int islong;
@@ -57,11 +60,11 @@ int find_datablk(struct zfs_superblock *sblk,const char *fname){
 		if(islong == 1){
 			namelen = getc(f);
 			i+=(namelen + 12);
-			fseek(f,fds * 512 + i,SEEK_SET);
+			fseek(f,sblk.fds * 512 + i,SEEK_SET);
 		}
 		else{
 			i+=21;
-			fseek(f,fds * 512 + i,SEEK_SET);
+			fseek(f,sblk.fds * 512 + i,SEEK_SET);
 		}
 	}
 	if(found == 0){
@@ -75,7 +78,7 @@ int find_datablk(struct zfs_superblock *sblk,const char *fname){
 	//printf(".\n");
 	return i;
 }
-int write_datablk(struct zfs_superblock *sblk,struct data *d,const char *fname,int loc){
+int write_datablk(struct zfs_superblock sblk,struct data *d,const char *fname,int loc){
 	FILE *f;
 	//printf("Allocating memory\n");
 	/*printf("Allocating %d bytes\n",get_end(fname));
@@ -91,7 +94,7 @@ int write_datablk(struct zfs_superblock *sblk,struct data *d,const char *fname,i
 
 	//printf("Writing buffer\n");
 	//write_buf(buf,fname);
-	fseek(f,sblk->fds * 512 + loc,SEEK_SET);
+	fseek(f,sblk.fds * 512 + loc,SEEK_SET);
 	fputc(1,f);
 	fputc(d->islongname,f);
 	if(d->islongname)
@@ -122,7 +125,7 @@ int write_file(const char *buf,struct data *d,const char *fname){
 	int pos = d->startingblock * 512;
 	int end = d->endingblock * 512;
 	FILE *f;
-	if(!(f = fopen(fname,"wb"))){
+	if(!(f = fopen(fname,"r+b"))){
 		printf("Couldn't open %s for writing",fname);
 		return -1;
 	}
@@ -164,7 +167,7 @@ int read_buf(char *buf,const char *name){
 }
 int write_buf(char *buf,const  char * name){
 	FILE *f;
-	if(!(f = fopen(name,"wb"))){
+	if(!(f = fopen(name,"r+b"))){
 		printf("Couldn't open %s for writing!\n",name);
 		return -1;
 	}
@@ -199,25 +202,31 @@ int read_file(char *buf,struct data *d,const char *fname){
 	fclose(f);
 	return 1;
 }
-int *find_unalloc_block(const char *filename,int numofblock,int start_blk){
+int *find_unalloc_block(const char *filename,int numofblock,struct zfs_superblock superblk){
+	//while(1) { }
+	int start_blk = superblk.fds;
+	printf("[filename] %s [numofblock] %d [start_blk] %d\n",filename,numofblock,start_blk);
 	int i = 0;
 	FILE *f;
 	if(!(f = fopen(filename,"rb"))){
 		printf("Couldnt open %s for reading", filename);
 		return -1;
 	}
+	printf("Seeking\n");
 	fseek(f,start_blk * 512,SEEK_SET);
+	printf("Initialising variables\n");
 	int needed = numofblock;
 	int got = 0;
 	int c = 0xFF;
 	int start;
 	int end;
-	int *ret = malloc(3);
+	int *ret = malloc(80);
 	ret[0] = -1;
 	ret[1] = -1;
+	int orig = start_blk * 512;
 	int pos = start_blk * 512;
 	while((c = fgetc(f)) != EOF){
-		printf(".");
+		//printf(".");
 		if(got == needed && c == 0){
 			end = pos;
 			ret[1] = end;
@@ -245,7 +254,9 @@ int *find_unalloc_block(const char *filename,int numofblock,int start_blk){
 	}
 	return -1;
 }
-struct data *parse_file(char *buf,const char *filename,char *_filename,struct zfs_superblock *sblk){
+struct data *parse_file(char *buf,const char *filename,char *_filename){
+	struct zfs_superblock *sblk = malloc(sizeof(struct zfs_superblock *));
+	parse_superblock(filename,sblk);
 	int end = 0;
 	while(1){
 		if(buf[end] == 0x0F && buf[end + 1] == 'E' && buf[end + 2] == 0x0F && buf[end + 3] == 'O' && buf[end + 4] == 0x0F && buf[end + 5] == 'F' && buf[end + 6] == 0x0F)
@@ -253,13 +264,13 @@ struct data *parse_file(char *buf,const char *filename,char *_filename,struct zf
 		end++;
 	}
 	end--;
-	printf("[end] %d\n",end);
+	printf("[end] %d [sblk.fds] %d\n",end,sblk->fds);
 	struct data *ret = malloc(sizeof(struct data *));
 	int *se = malloc(80);
 	if((end % 512) == 0)
-		se = find_unalloc_block(filename,end,sblk->fds);
-//	else
-//		se = find_unalloc_block(filename,end ,sblk->fds);
+		se = find_unalloc_block(filename,end,*sblk);
+	else
+		se = find_unalloc_block(filename,end,*sblk);
 	if(!(se)){
 		printf("Error finding unallocated block!\n");
 		return -1;
@@ -274,13 +285,26 @@ struct data *parse_file(char *buf,const char *filename,char *_filename,struct zf
 		ret->islongname = 0;
 	if(ret->islongname)
 		ret->namelen = strlen(filename);
+	else
+		ret->namelen = 0;
 	ret->name = _filename;
 	ret->type = TYPE_FILE;
 	ret->blockpos = ret->startingblock;
 	return ret;
 }
-struct zfs_superblock *parse_superblock(char *buf){
-	struct zfs_superblock *ret = malloc(sizeof(struct zfs_superblock *));
+struct zfs_superblock *parse_superblock(char *name,struct zfs_superblock *ret){
+	//struct zfs_superblock *ret = malloc(sizeof(struct zfs_superblock *));
+	FILE *f = fopen(name,"r+b");
+	if(!(f))
+		return -1;
+	char *buf = malloc(2048);
+	//fread(buf,1050,1,f);
+	int c;
+	for(int i = 0;i < 1050 && c != EOF;i++){
+		c = fgetc(f);
+		buf[i] = c;
+	}
+	//fread(f,buf,1050);
 	ret->allocblk = buf[1024] << 24 | buf[1025] << 16 | buf[1026] << 8 | buf[1027];
 	ret->unallocblk = buf[1028] << 24 | buf[1029] << 16 | buf[1030] << 8 | buf[1031];
 	ret->tblk = ret->allocblk + ret->unallocblk;
@@ -289,6 +313,7 @@ struct zfs_superblock *parse_superblock(char *buf){
 	ret->numoff = buf[1040] << 24 | buf[1041] << 16 | buf[1042] << 8 | buf[1043];
 	ret->numofd = buf[1044] << 24 | buf[1045] << 16 | buf[1046] << 8 | buf[1047];
 	ret->fds = buf[1048] << 8 | buf[1049];
+	free(buf);
 	return ret;
 	/*ret->allocblk = (buf[1024] << 16) * 10000 | (buf[1025] << 8) * 10000 | buf[1026] * 10000 | buf[1027] << 16 | buf[1028] << 8 | buf[1029];
 	ret->unallocblk = (buf[1030] << 16) * 10000 | (buf[1031] << 8) * 10000 | buf[1032] * 10000 | buf[1033] << 16 | buf[1034] << 8 | buf[1035];
@@ -411,16 +436,19 @@ int main(int argc, char* argv[]){
 	buf = malloc(get_end(argv[1]));
 	read_buf(buf,argv[1]);
 	printf("Parsing superblock\n");
-	struct zfs_superblock *sblk = parse_superblock(buf);
+	struct zfs_superblock *sblk = malloc(sizeof(struct zfs_superblock *));
+	parse_superblock(argv[1],sblk);
+	if(!(sblk))
+		return -1;
 	printf("Dumping: [allocblk] %d [unallocblk] %d [tblk]   %d\n         [dataresv] %d [rootfsblk]  %d [numoff] %d\n         [numofd]   %d [fds]        %d\n",sblk->allocblk,sblk->unallocblk,sblk->tblk,sblk->dataresv,sblk->rootfsblk,sblk->numoff,sblk->numofd,sblk->fds);
 	printf("Finding datablock\n");
-	int db = find_datablk(sblk,argv[1]);
+	int db = find_datablk(*sblk,argv[1]);
 	if(db < 0){
 		printf("Error finding datablock!\n");
 		return -1;
 	}
 	printf("freeing buffer\n");
-	//free(buf);
+	free(buf);
 	printf("Allocating memory\n");
 	buf = malloc(get_end(argv[2]));
 	if(!(buf)){
@@ -432,7 +460,7 @@ int main(int argc, char* argv[]){
 		return -1;;
 	}
 	printf("Parsing file\n");
-	struct data *d = parse_file(buf,argv[1],argv[2],sblk);
+	struct data *d = parse_file(buf,argv[1],argv[2]);
 	if(!(d)){
 		printf("Error parsing file!\n");
 		return -1;
@@ -440,7 +468,7 @@ int main(int argc, char* argv[]){
 	printf("Dumping: [alloc]           %d   [islongname]   %d   [namelen]   %d   [name]     %s\n         [type]            %d   [offset]       %d   [endingpos] %d   [blockpos] %d\n         [startingblock]   %d   [endingblock]  %d\n"
 	,d->alloc,d->islongname,d->namelen,d->name,d->type,d->offset,d->endingpos,d->blockpos,d->startingblock,d->endingblock);
 	printf("Writing datablock\n");
-	if(!(write_datablk(sblk,d,argv[1],db))){
+	if(!(write_datablk(*sblk,d,argv[1],db))){
 		printf("Error writing datablock!\n");
 		return -1;
 	}
